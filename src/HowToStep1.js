654e87af043fb96622f4adc28be3d26cf913b40e
Matt Allbeury <mallbeury@mac.com>
2018-12-13 12:51:01 +0100

    HTG edits

--- src/HowToStep1.js
+++ src/HowToStep1.js
@@ -5,15 +5,15 @@ const HowToStep1 = (props) => {
   return (
     <div className="howToStep">
       <div className="content">
-        <div className="text">
-        <h1>{props.title}</h1>
-        <h2>{props.subtitle}</h2>
-        {props.description}
-        </div>
         <div className="image">
         <ScrollAnimation key={props.step} animateIn="fadeIn" animateOut="fadeOut">
         <img src="https://www.mountainrush.co.uk/static-assets/images/wwf-uk/mobile_big.png" />
         </ScrollAnimation>
+        </div>
+        <div className="text">
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
+        {props.description}
         </div>
       </div>
     </div>
--- src/HowToStep3.js
+++ src/HowToStep3.js
@@ -10,6 +10,11 @@ export default class HowToStep3 extends React.Component {
     return (
       <div className="howToStep">
         <div className="content">
+          <div className="image">
+          <ScrollAnimation key={this.props.step} animateIn="fadeIn" animateOut="fadeOut">
+          <img src="https://www.mountainrush.co.uk/static-assets/images/wwf-uk/mobile_big.png" />
+          </ScrollAnimation>
+          </div>
           <div className="text">
             <h1>{this.props.title}</h1>
             <h2>{this.props.subtitle}</h2>
@@ -19,11 +24,6 @@ export default class HowToStep3 extends React.Component {
             <div>Sign up</div><div><i className="fa fa-arrow-right"></i></div>
             </button>
             </div>
-          </div>
-          <div className="image">
-          <ScrollAnimation key={this.props.step} animateIn="fadeIn" animateOut="fadeOut">
-          <img src="https://www.mountainrush.co.uk/static-assets/images/wwf-uk/mobile_big.png" />
-          </ScrollAnimation>
           </div>
         </div>
       </div>
