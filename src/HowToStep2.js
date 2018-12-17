9874b7f096a5214334d7b44c45a52e984fcadbf9
Matt Allbeury <mallbeury@mac.com>
2018-12-17 21:37:40 +0100

    Update mobile view

--- src/HowToStep2.js
+++ src/HowToStep2.js
@@ -10,18 +10,36 @@ const HowToStep2 = (props) => {
       <div className="content">
         <div className="embelish left-background"></div>
 
-        <div className="col col-left">
-          <div className="text">
-            <div className="animate">
-            <h1>{props.stepContent.title}</h1>
-            <h2>{props.stepContent.subtitle}</h2>
-            {props.stepContent.description.replace('%s', props.defContent.strava_app)}
+        <div className="visible-xs">
+          <div className="col col-left">
+            <div className="image"><img className="animate" src="http://staging.mountainrush.co.uk/static-assets/images/temp/step2.png" /></div>
+          </div>
+          <div className="col col-right">
+            <div className="text">
+              <div className="animate">
+              <h1>{props.stepContent.title}</h1>
+              <h2>{props.stepContent.subtitle}</h2>
+              {props.stepContent.description.replace('%s', props.defContent.strava_app)}
+              </div>
             </div>
           </div>
         </div>
-        <div className="col col-right">
-          <div className="image"><img className="animate" src="http://staging.mountainrush.co.uk/static-assets/images/temp/step2.png" /></div>
+
+        <div className="hidden-xs">
+          <div className="col col-left">
+            <div className="text">
+              <div className="animate">
+              <h1>{props.stepContent.title}</h1>
+              <h2>{props.stepContent.subtitle}</h2>
+              {props.stepContent.description.replace('%s', props.defContent.strava_app)}
+              </div>
+            </div>
+          </div>
+          <div className="col col-right">
+            <div className="image"><img className="animate" src="http://staging.mountainrush.co.uk/static-assets/images/temp/step2.png" /></div>
+          </div>
         </div>
+
       </div>
 
       </ScrollAnimation>
