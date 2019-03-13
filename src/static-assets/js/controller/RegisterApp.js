a5fb328c234a08940f8ce6c47a11c12c3ca32484
Matt Allbeury <mallbeury@mac.com>
2019-03-13 14:56:03 +0100

    Token test

--- src/static-assets/js/controller/RegisterApp.js
+++ src/static-assets/js/controller/RegisterApp.js
@@ -124,6 +124,7 @@ define([
 //          PLAYER_TOKEN = '9b69f66ee9a7d20702a5b9771322388df8cb886a'; // MR - Matt
 //        PLAYER_TOKEN = 'b8a1bc6ca786c95f1e639c42615320782d8a9d22'; // MR - Trailburning
 //          PLAYER_TOKEN = '3e43a998394b56e0616e3ccd4085ba762f83e861'; // CFYW - Amelia
+//          PLAYER_TOKEN = '3e43a998394b56e0616e3ccd4085ba762f83e861'; // CFYW - Amelia
 //        } 
 //        changeState(STATE_GAME_CREATE);
 //        return;
