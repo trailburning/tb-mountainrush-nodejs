c6c839a0437585f920495f4a5a96e412f4a0909c
Matt Allbeury <mallbeury@mac.com>
2019-03-21 12:26:26 +0100

    Edit

--- src/static-assets/js/views/PlayerActivityPhotoView.js
+++ src/static-assets/js/views/PlayerActivityPhotoView.js
@@ -81,7 +81,7 @@ define([
       $('.post', this.options.elParent).each(function(index){
         console.log('check:'+$(this).attr('data-uploaded-time'));
         // is post older?
-        if (!elFoundNextPost && (Number($(this).attr('uploaded_at_time')) > Number(self.options.model.get('uploaded_at_time')))) {
+        if (!elFoundNextPost && (Number($(this).attr('data-uploaded-time')) > Number(self.options.model.get('uploaded_at_time')))) {
           console.log('INSERT HERE');
           elFoundNextPost = $(this);
         }
