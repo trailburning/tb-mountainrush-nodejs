307f9d41c7d3e44deb15d877d5250a58bdbdf635
Matt Allbeury <mallbeury@mac.com>
2019-07-19 18:25:34 +0200

    test

--- src/static-assets/js/controller/GameDetailsApp.js
+++ src/static-assets/js/controller/GameDetailsApp.js
@@ -380,7 +380,10 @@ define([
         var nPhotos = $('.post.active', elPhotos).length;
 
         // as we get photos we can hide the blank placeholders
+        console.log('photos:'+nPhotos);
+
         $('.post.inactive', elPhotos).each(function(index) {
+          console.log(index+' : '+nPhotos);
           if (index < nPhotos) {
             $(this).hide();
           }
