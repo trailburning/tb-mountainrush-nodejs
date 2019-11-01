dacc96766c85e14fedc08c79a1ae74fc1ecca0fd
Matt Allbeury <mallbeury@mac.com>
2019-11-01 13:35:00 +0100

    Initial Stripe

--- src/static-assets/js/views/RegisterWelcomePaywallView.js
+++ src/static-assets/js/views/RegisterWelcomePaywallView.js
@@ -60,9 +60,93 @@ define([
 
     render: function(options){
       var self = this;
+      
+      // Submit the form with the token ID.
+      function stripeTokenHandler(token) {
+        // Insert the token ID into the form so it gets submitted to the server
+        var form = document.getElementById('payment-form');
+        var hiddenInput = document.createElement('input');
+        hiddenInput.setAttribute('type', 'hidden');
+        hiddenInput.setAttribute('name', 'stripeToken');
+        hiddenInput.setAttribute('value', token.id);
+        form.appendChild(hiddenInput);
+
+        // Submit the form
+//        form.submit();
+
+        var jsonData = {token: token.id, amount: 10, name: 'Hello World'};
+        console.log(jsonData);
+
+        var url = GAME_API_URL + "player/" + self.jsonFields.playerID + '/paywall/payment';
+        console.log(url);
+
+        $.ajax({
+          type: 'post',
+          dataType: 'json',
+          url: url,
+          data: JSON.stringify(jsonData),
+          error: function(data) {
+            console.log('error');
+            console.log(data);  
+          },
+          success: function(data) {
+            console.log('success');
+            console.log(data);
+
+          }
+        });
+      }
+
+      function setupStripe() {
+        var stripe = Stripe('pk_test_wBccDPYUjxKXgvanR0EaSgsC');
+        var elements = stripe.elements();
+
+        // Custom styling can be passed to options when creating an Element.
+        // (Note that this demo uses a wider set of styles than the guide below.)
+        var style = {
+          base: {
+            color: '#32325d',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+              color: '#aab7c4'
+            }
+          },
+          invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
+          }
+        };
+
+        // Create an instance of the card Element.
+        var card = elements.create('card', {style: style});
+
+        // Add an instance of the card Element into the `card-element` <div>.
+        card.mount('#card-element');
+
+        // Handle form submission.
+        var form = document.getElementById('payment-form');
+        form.addEventListener('submit', function(event) {
+          event.preventDefault();
+
+          stripe.createToken(card).then(function(result) {
+            if (result.error) {
+              // Inform the user if there was an error.
+              var errorElement = document.getElementById('card-errors');
+              errorElement.textContent = result.error.message;
+            } else {
+              // Send the token to your server.
+              stripeTokenHandler(result.token);
+            }
+          });
+        });
+      }
 
       $(this.el).html(this.template({ campaign: options.jsonCampaign, player: this.jsonPlayer }));
 
+      setupStripe();
+/*
       var elForm = $('form', $(this.el));
       elForm.submit(function(evt){
         evt.preventDefault();
@@ -75,7 +159,7 @@ define([
           self.updatePreferences(fPaywallAmount);
         }
       });
-
+*/
       return this;
     }
 
