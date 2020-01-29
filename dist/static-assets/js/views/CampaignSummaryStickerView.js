e43a8c4b8e0aed618dcdccacee12bb8fd5b16b20
Matt Allbeury <mallbeury@mac.com>
2020-01-29 15:06:49 +0100

    Update campaign summary

--- dist/static-assets/js/views/CampaignSummaryStickerView.js
+++ dist/static-assets/js/views/CampaignSummaryStickerView.js
@@ -30,10 +30,29 @@ define([
         return;
       }
 
+      var fTotalRaised = 0;
+      var strCurrency = '';
+
       var jsonContent = this.result;
+
+      if (jsonContent.campaign_players_paywall.length) {
+        strCurrency = jsonContent.campaign_players_paywall[0].fundraising_currency_symbol;
+        fTotalRaised += Number(jsonContent.campaign_players_paywall[0].total_paywall_amount);
+      }
+
+      if (jsonContent.campaign_donations.length) {
+        strCurrency = jsonContent.campaign_donations[0].fundraising_currency_symbol;
+        fTotalRaised += Number(jsonContent.campaign_donations[0].total_donations);
+      }
+
       if (jsonContent.campaign_challenges.length) {
+        strCurrency = jsonContent.campaign_challenges[0].fundraising_currency_symbol;
+        fTotalRaised += Number(jsonContent.campaign_challenges[0].total_fundraising_raised);
         jsonContent.total_ascent_formatted = numeral(Math.round(jsonContent.campaign_challenges[0].total_ascent)).format('0,0');
       }
+
+      jsonContent.fundraising_currency_symbol = strCurrency;
+      jsonContent.total_raised = fTotalRaised;
 
       $(this.el).html(this.template(jsonContent));
 
